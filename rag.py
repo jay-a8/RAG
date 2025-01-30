42f367932747f0cb7d4b1ef12467ad865bc70d25
Xindi <xindizheng123@gmail.com>
2025-01-30 10:24:44 -0700

    fixed chunk size

--- rag.py
+++ rag.py
@@ -4,8 +4,8 @@ from langchain_core.vectorstores import InMemoryVectorStore
 from langchain_ollama import OllamaLLM
 from langchain.chains import create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
-
 from langchain_core.prompts import ChatPromptTemplate
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 from numpy import dot
 from numpy.linalg import norm
@@ -20,14 +20,31 @@ from numpy.linalg import norm
 # 4. stored the embeds into vector store
 # 5. given a query, embed query, and find relevant embeded docs in the vectoratore 
 # 6. asking query with relevant data to LLM model
-#  
+# 
 
 import os
 print(os.path.exists("./data.txt"))  #check existence
 
+
+
 # load text, encoding with utf-8
 loader = TextLoader("./data.txt", encoding="utf-8")
-doc_list = loader.load_and_split()
+docs = loader.load()
+
+
+total_length = sum(len(doc.page_content) for doc in docs)
+num_chunks = max(1, len(docs) * 5)
+dynamic_chunk_size = max(100, total_length // num_chunks)
+
+
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=dynamic_chunk_size,  chunk_overlap=0)
+
+doc_list = text_splitter.split_documents(docs)
+
+print(doc_list[0].page_content)
+
+
+
 # print(doc_list[0])
 
 embeddings = OllamaEmbeddings(model="llama3.2")
@@ -43,22 +60,25 @@ vectorstore = InMemoryVectorStore.from_documents(
 query = "who is mark"
 
 # Use the vectorstore as a retriever
-retriever = vectorstore.as_retriever()
+retriever = vectorstore.as_retriever(search_type="similarity", search_kwargs={"k": 1}) 
 
 # Retrieve the most similar text
 retrieved_documents = retriever.invoke(query)
-
-# query_embedding = embeddings.embed_query(query)
-# print(query_embedding)
-
-# cosine_similarity = dot(query_embedding, text_embedding) / (norm(query_embedding) * norm(text_embedding))
-
-# print(f"Query 和 文档的相似度: {cosine_similarity:.4f}")
 
 # show the retrieved document's content
 relavent_doc = retrieved_documents[0].page_content
 print(relavent_doc)
 
+
+query_embedding = embeddings.embed_query(query)
+# print(query_embedding)
+
+
+doc_embedding = embeddings.embed_query(relavent_doc)
+
+cosine_similarity = dot(query_embedding, doc_embedding) / (norm(query_embedding) * norm(doc_embedding))
+
+print(f"similiarty between query and doc: {cosine_similarity:.4f}")
 
 llm = OllamaLLM(model="llama3.2")
 # system_prompt = (
@@ -76,11 +96,6 @@ llm = OllamaLLM(model="llama3.2")
 
 # response = chain.invoke({"input": query})
 # print(response)
-
-test = "You are a consice AI, you will reply my question with my provided data. This is the relavent documentation for this query, pls use it for your answer: " + relavent_doc + "This is the query:" + query
-print(test)
-
-print("=====================================================")
 
 test = "You are a consice AI, you will reply my question with my provided data. This is the relavent documentation for this query, pls use it for your answer: " + relavent_doc + "This is the query:" + query
 # print(test)
