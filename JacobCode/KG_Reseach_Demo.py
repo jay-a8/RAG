aace8bf223e166ff7fa9ced70464717c8bd1c779
Xindi <xindizheng123@gmail.com>
2025-03-03 13:08:38 -0700

    KG_Research_Demo.py is available

--- JacobCode/KG_Reseach_Demo.py
+++ JacobCode/KG_Reseach_Demo.py
@@ -1,40 +1,40 @@
-# from kg_builder import docs_to_kg
-# import json
-# import os
-# import networkx as nx
-# import pickle
+from kg_builder import docs_to_kg
+import json
+import os
+import networkx as nx
+import pickle
 
-# import matplotlib.pyplot as plt
-
-
-# # This is an array of all documents you want to be put into the graph
-# # Works with .txt, .pdf, and .json of the format [{"image": <image name>, "description":<text description of image>}]
-# txt_file_paths = ["test.txt", "test2.txt"]
-
-# # This is the file you want the graph to be saved to
-# graph_file = "Test.json"
-
-# # This is the file where the chunked documents will be stored
-# pickle_file = "Test.pkl"
+import matplotlib.pyplot as plt
 
 
-# # Create Graph
-# G, chunks = docs_to_kg(txt_file_paths, graph_file, pickle_file)
+# This is an array of all documents you want to be put into the graph
+# Works with .txt, .pdf, and .json of the format [{"image": <image name>, "description":<text description of image>}]
+txt_file_paths = ["test.txt", "test2.txt"]
 
-# nx.node_link_data(G, edges="edges")
+# This is the file you want the graph to be saved to
+graph_file = "Test.json"
 
-# # Visualize the graph
-# plt.figure(figsize=(24, 20))
-# pos = nx.spring_layout(G, k=0.5, iterations=100)
-# nx.draw(G, pos, with_labels=True, node_color='lightblue', font_weight='bold', node_size=300, font_size=5)
-
-# # Write the relationships between nodes, gets ugly fast with large KGs
-# edge_labels = nx.get_edge_attributes(G, 'type')
-# nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels)
+# This is the file where the chunked documents will be stored
+pickle_file = "Test.pkl"
 
 
-# plt.title("Knowledge Graph Visualization")
-# plt.show()
+# Create Graph
+G, chunks = docs_to_kg(txt_file_paths, graph_file, pickle_file)
+
+nx.node_link_data(G, edges="edges")
+
+# Visualize the graph
+plt.figure(figsize=(24, 20))
+pos = nx.spring_layout(G, k=0.5, iterations=100)
+nx.draw(G, pos, with_labels=True, node_color='lightblue', font_weight='bold', node_size=300, font_size=5)
+
+# Write the relationships between nodes, gets ugly fast with large KGs
+edge_labels = nx.get_edge_attributes(G, 'type')
+nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels)
+
+
+plt.title("Knowledge Graph Visualization")
+plt.show()
 
 
 from kg_builder import reasoning, KG_RAG, gpt_call
